7424ad1cfbd7b38bf63ddd8cf572223d65c58081
Rich Leach <rich@cfsnap.com>
2024-06-11 14:30:08 -0600

    updated link in myNav.tsx

--- app/components/myNavbar.tsx
+++ app/components/myNavbar.tsx
@@ -1,13 +1,10 @@
 import { Navbar, NavbarItem, NavbarSection } from "./navbar";
-import { Logo } from "../../public/images/pp-script-logo.png";
 import Link from "next/link";
 
 export default function MyNavBar() {
   return (
     <Navbar>
-      <Link href="/" aria-label="Home">
-        
-      </Link>
+      <Link href="/" aria-label="Home"></Link>
       <NavbarSection>
         <NavbarItem href="/" current>
           Home
